cf4e4171fe449931a5cf42dcb4ad8dcabd0abe00
Sreeja-Yetukuri <108686592+Sreeja-Yetukuri@users.noreply.github.com>
2022-07-22 22:33:51 +0530

    Create add-event.componet.js

---
+++ add-event.componet.js
@@ -0,0 +1,258 @@
+import React, { Component } from "react";
+import EventDataService from "../services/event.service";
+import { TextData } from "./Textfield";
+import { Formik, Form } from "formik"
+import "date-fns";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class AddEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeVenueName = this.onChangeVenueName.bind(this);
+    this.onChangeVenueAddress = this.onChangeVenueAddress.bind(this);
+    this.onChangeDateOfEvent = this.onChangeDateOfEvent.bind(this);
+    this.onChangeStartTime = this.onChangeStartTime.bind(this);
+    this.onChangeEndTime = this.onChangeEndTime.bind(this);
+    this.onChangeAvailability = this.onChangeAvailability.bind(this);
+    this.onChangePrice = this.onChangePrice.bind(this);
+    this.saveEvent = this.saveEvent.bind(this);
+    this.newEvent = this.newEvent.bind(this);
+
+    this.state = {
+      id: null,
+      title: "",
+      description: "",
+      venueName: "",
+      venueAddress: "",
+      dateOfEvent: "",
+      startTime: "",
+      endTime: "",
+      availability: "",
+      price: "",
+      published: false,
+      show: false,
+      date: "",
+
+      submitted: false
+    };
+  }
+
+ 
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+  // 
+  onChangeVenueName(e) {
+    this.setState({
+      venueName: e.target.value
+    });
+  }
+
+  onChangeVenueAddress(e) {
+    this.setState({
+      venueAddress: e.target.value
+    });
+  }
+
+  onChangeDateOfEvent(dateOfEvent) {
+    this.setState({
+      dateOfEvent
+    });
+  }
+
+  onChangeStartTime(e) {
+    this.setState({
+      startTime: e.target.value
+    });
+  }
+
+  onChangeEndTime(e) {
+    this.setState({
+      endTime: e.target.value
+    });
+  }
+
+  onChangeAvailability(e) {
+    this.setState({
+      availability: e.target.value
+    });
+  }
+
+  onChangePrice(e) {
+    this.setState({
+      price: e.target.value
+    });
+  }
+  // 
+  saveEvent() {
+    var data = {
+      title: this.state.title,
+      description: this.state.description,
+      venueName: this.state.venueName,
+      venueAddress: this.state.venueAddress,
+      dateOfEvent: this.state.dateOfEvent,
+      startTime: this.state.startTime,
+      endTime: this.state.endTime,
+      availability: this.state.availability,
+      price: this.state.price
+    };
+
+    EventDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          title: response.data.title,
+          description: response.data.description,
+          venueName: response.data.venueName,
+          venueAddress: response.data.venueAddress,
+          dateOfEvent: response.data.dateOfEvent,
+          startTime: response.data.startTime,
+          availability: response.data.availability,
+          price: response.data.price,
+          published: response.data.published,
+
+          submitted: true
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newEvent() {
+    this.setState({
+      id: null,
+      title: "",
+      description: "",
+      venueName: "",
+      venueAddress: "",
+      dateOfEvent: "",
+      startTime: "",
+      endTime: "",
+      availability: "",
+      price: "",
+      published: false,
+
+      submitted: false
+    });
+  }
+
+
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newEvent}>
+              Create
+            </button>
+          </div>
+        ) : (
+          <div>
+            <Formik initialValues={this.state} onSubmit={this.saveEvent}>
+              <div>
+                <Form >
+                  <TextData control='input' type="text" className="form-control" label="Event Name"
+                    id="title"
+                    required
+                    value={this.state.title}
+                    onChange={this.onChangeTitle}
+                    name="title" />
+                  <TextData control='input' type="text"
+                    className="form-control"
+                    id="venueName"
+                    required
+                    value={this.state.venueName}
+                    onChange={this.onChangeVenueName}
+                    name="venueName"
+                    label="Venue Name" />
+                  <TextData control='input' type="text"
+                    className="form-control"
+                    id="venueName"
+                    required
+                    label='Venue Address'
+                    value={this.state.venueAddress}
+                    onChange={this.onChangeVenueAddress}
+                    name="venueAddress" />
+                  <hr />
+                  <label>Date of event</label>
+                  <DatePicker
+                    className="form-control"
+                    id="dateOfEvent"
+                    required
+                    value={this.state.dateOfEvent}
+                    selected={this.state.dateOfEvent}
+                    onChange={this.onChangeDateOfEvent}
+                    name="dateOfEvent"
+                    dateFormat="dd/MM/yyyy"
+                    minDate={new Date()}
+                    />
+                  <TextData control='input' type="text"
+                    className="form-control"
+                    id="startTime"
+                    required
+                    label='Start Time'
+                    value={this.state.startTime}
+                    onChange={this.onChangeStartTime}
+                    name="startTime" />
+                  <TextData control='input' type="text"
+                    className="form-control"
+                    id="endTime"
+                    required
+                    label='End Time'
+                    value={this.state.endTime}
+                    onChange={this.onChangeEndTime}
+                    name="endTime" />
+                  <hr />
+                  <TextData control='input' type="text"
+                    className="form-control"
+                    id="description"
+                    required
+                    label='About The Event'
+                    value={this.state.description}
+                    onChange={this.onChangeDescription}
+                    name="description" />
+                  <hr />
+                  <TextData control='input' type="number"
+                    className="form-control"
+                    id="availability" 
+                    required
+                    label='Availability' min="0" oninput="this.value=!!(this.value && Math.abs(this.value)>=0?Math.abs(this.value):null"
+                    value={this.state.availability}
+                    onChange={this.onChangeAvailability}
+                    name="availability" />
+                  <TextData control='input' type="number"
+                    className="form-control"
+                    id="price"   min="0" oninput="this.value=!!(this.value && Math.abs(this.value)>=0?Math.abs(this.value):null"
+                    required
+                    label='Price'
+                    value={this.state.price}
+                    onChange={this.onChangePrice}
+                    name="price" />
+                  <hr />
+                  <button type='submit' className="btn btn-success">Submit</button>
+                </Form>
+              </div>
+            </Formik>
+          </div>
+        )}
+      </div>
+
+    );
+  }
+}
